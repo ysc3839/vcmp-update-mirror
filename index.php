7c759f78e73fdb83263208ffa726e51ab88fc575
ysc3839 <ysc3839@163.com>
2016-07-23 21:05:47 +0800

    First.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/mdl/material.min.css">
+<script src="/mdl/material.min.js"></script>
+<title>VCMP Update Mirror</title>
+</head>
+<body>
+<div class="mdl-typography--text-center">
+<h1>VCMP Update Mirror</h1>
+<h3>In progress...</h3>
+<div class="mdl-progress mdl-js-progress mdl-progress__indeterminate mdl-shadow--4dp" style="margin: auto; width: 50%; margin-bottom: 20px;"></div>
+<a target="_blank" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary" href="https://github.com/ysc3839/vcmp-update-mirror">View on GitHub</a>
+</div>
+</body>
+</html>
