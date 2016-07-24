886fa1568780f63237b899f5723de4ae49ab594a
ysc3839 <ysc3839@163.com>
2016-07-24 11:53:01 +0800

    UI.

--- index.php
+++ index.php
@@ -1,17 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 <link rel="stylesheet" href="/mdl/material.min.css">
 <script src="/mdl/material.min.js"></script>
+<style>
+.progress {
+	margin: auto;
+	width: 50%;
+	margin-bottom: 20px;
+}
+.inline-block {
+	display: inline-block;
+}
+.icon-download {
+	background-size: contain;
+    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAQAAAD9CzEMAAAAV0lEQVR4Ae3QMQ7EQAzDQD2dP3dKF8E1BpjioHEvYjf1kZof10ADDTRgBsgcDjeB+wrcjyJHWPMLa35hzS+s+YU1v3Dn3wkiwJ3fBBER8hdqbncMNFBVDxodwlpdA7aUAAAAAElFTkSuQmCC");
+    height: 24px;
+}
+.footer {
+	padding-top: 5px;
+}
+</style>
 <title>VCMP Update Mirror</title>
 </head>
 <body>
-<div class="mdl-typography--text-center">
-<h1>VCMP Update Mirror</h1>
-<h3>In progress...</h3>
-<div class="mdl-progress mdl-js-progress mdl-progress__indeterminate mdl-shadow--4dp" style="margin: auto; width: 50%; margin-bottom: 20px;"></div>
-<a target="_blank" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary" href="https://github.com/ysc3839/vcmp-update-mirror">View on GitHub</a>
+<div class="mdl-cell mdl-cell--12-col mdl-typography--text-center">
+<span class="mdl-color-text--primary"><h1>VCMP Update Mirror</h1>
+<h3>In progress...</h3></span>
+<div class="mdl-progress mdl-js-progress mdl-progress__indeterminate mdl-shadow--4dp progress"></div>
+<div class="mdl-color-text--grey-600 inline-block"><table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+	<thead>
+	<tr>
+    	<th class="mdl-data-table__cell--non-numeric">Version</th>
+    	<th class="mdl-data-table__cell--non-numeric">Build</th>
+    	<th class="mdl-data-table__cell--non-numeric">Date</th>
+		<th>Download</th>
+    </tr>
+	</thead>
+	<tbody>
+	<tr>
+    	<td class="mdl-data-table__cell--non-numeric">04rel004</td>
+    	<td class="mdl-data-table__cell--non-numeric">578F0CAD</td>
+    	<td class="mdl-data-table__cell--non-numeric">2016-07-20 07:07:31</td>
+		<td><a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" download=""><i class="material-icons icon-download"></i></a></td>
+    </tr>
+	<tr>
+    	<td class="mdl-data-table__cell--non-numeric">04rel003</td>
+    	<td class="mdl-data-table__cell--non-numeric">571C093D</td>
+    	<td class="mdl-data-table__cell--non-numeric">2016-04-24 01:04:46</td>
+		<td><a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" download=""><i class="material-icons icon-download"></i></a></td>
+    </tr>
+	<tr>
+    	<td class="mdl-data-table__cell--non-numeric">04rel002</td>
+    	<td class="mdl-data-table__cell--non-numeric">55986E38</td>
+    	<td class="mdl-data-table__cell--non-numeric">2015-07-05 01:07:37</td>
+		<td><a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" download=""><i class="material-icons icon-download"></i></a></td>
+    </tr>
+	</tbody>
+</table></div>
+<div class="footer"><a target="_blank" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="https://github.com/ysc3839/vcmp-update-mirror">View on GitHub</a></div>
 </div>
 </body>
 </html>
